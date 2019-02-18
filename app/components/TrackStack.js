3be3d4a20c6b90123ea2f39bbb66e85bb7f3c66d
Clay Tercek <code@claytercek.com>
2019-02-17 20:00:45 -0500

    updated transition animation

--- app/components/TrackStack.js
+++ app/components/TrackStack.js
@@ -110,7 +110,7 @@ class TrackView extends Component {
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (e, gesture) => true,
       onPanResponderGrant: (e, gesture) => {
-        this.setState({ dragging: true });
+        // this.setState({ dragging: true });
       },
       onPanResponderMove: (e, gesture) => {
         if (!this.props.isBottom && gesture.dy > 5)
@@ -120,33 +120,50 @@ class TrackView extends Component {
         Animated.event([null, { dy: this.state.pan.y }])(e, gesture)
       },
       onPanResponderRelease: (e, gesture) => {
-        this.setState({ dragging: false });
-        if (!this.props.isBottom && gesture.dy < 20) {
-          Animated.spring(this.state.pan, {
-            toValue: { x: 0, y: 0 },
-            friction: 5
-          }).start();
-          
-          this.props.onStopDrag(null);
-        } else if (this.props.isBottom && gesture.dy > 20) {
-          Animated.spring(this.state.pan, {
-            toValue: { x: 0, y: 0 },
-            friction: 5
-          }).start();
-          
-          this.props.onStopDrag(null);
-        } else {
-          Animated.spring(this.state.pan, {
-            toValue: { x: 0, y: this.props.isBottom ? -110 : 110 },
-            friction: 5
-          }).start();
-          this.props.onStopDrag(this.props.track,this.props.i, this.props.isBottom);
-        }
+        this.onRelease(e,gesture, false);
+      },
+      onPanResponderTerminate: (e, gesture) => {
+        this.onRelease(e,gesture, true);
       }
       // adjusting delta value
     });
 
-    this.state.pan.setValue({ x: 0, y: 0 })
+    this.state.pan.setValue({ x: 0, y: 0 });
+    if (!this.props.dragFrom && (this.props.dragData.index == this.props.i ) && (this.props.transitioning)) {
+      let multiplier = (this.props.isBottom) ? 1 : -1;
+      let distance =( multiplier ) * this.props.dragData.dragDistance - (multiplier * (styles.trackView.height + styles.stack.marginTop));
+      console.log(distance);
+      this.animateIn(distance);
+    }
+  }
+
+  onRelease = (e, gesture, cancel) => {
+    this.setState({ dragging: false });
+    if (!this.props.isBottom &&( gesture.dy < 40 || cancel)) {
+      Animated.spring(this.state.pan, {
+        toValue: { x: 0, y: 0 },
+        friction: 5
+      }).start();
+      
+      this.props.onStopDrag(null);
+    } else if (this.props.isBottom && (gesture.dy > -40 || cancel)) {
+      Animated.spring(this.state.pan, {
+        toValue: { x: 0, y: 0 },
+        friction: 5
+      }).start();
+      
+      this.props.onStopDrag(null);
+    } else {
+      this.props.onStopDrag(this.props.track, this.props.i, this.props.isBottom, gesture.dy);
+    }
+  }
+
+  animateIn = (y) => {
+    this.state.pan.y.setValue(y);
+    Animated.spring(this.state.pan, {
+      toValue: { x: 0, y: 0 },
+      friction: 5
+    }).start();
   }
 
   render() {
@@ -155,15 +172,24 @@ class TrackView extends Component {
       zIndex: 2
     }
     var insertStyle = {}
-    if (!this.props.dragFrom && (this.props.dragData.index == this.props.i)) {
+    let fromOffset = (this.props.isBottom) ? this.props.offsets.listB : this.props.offsets.listA;
+    let toOffset = (this.props.isBottom) ? this.props.offsets.listA : this.props.offsets.listB;
+    let increment = (this.props.transitioning) ? 1 : 0;
+    if (!this.props.dragFrom && (this.props.dragData.index + fromOffset == this.props.i + toOffset - increment)) {
       insertStyle = {
         marginLeft: this.props.dragData.marginLeft,
-        zIndex: 0
+        zIndex: 0,
+        backgroundColor: '#FAA'
       }
     } if (this.props.dragFrom && (this.props.dragData.index == this.props.i - 1)) {
       insertStyle = {
         marginLeft: this.props.dragData.marginAdjacent,
         zIndex: 0
+      }
+    } if (!this.props.dragFrom && (this.props.dragData.index == this.props.i ) && (this.props.transitioning)) {
+      insertStyle = {
+        marginRight: -1 * (styles.trackView.width),
+        backgroundColor: '#F55'
       }
     }
 
@@ -189,24 +215,31 @@ class TrackStack extends Component {
         index: 0,
         marginLeft: new Animated.Value(0),
         marginAdjacent: new Animated.Value(0),
+        dragDistance: 0
       },
-      dragFromTop: true
+      offsets: {
+        listA: 0,
+        listB: 0
+      },
+      dragFromTop: true,
+      insertTransitioning: false
     };
   }
 
   onDrag = (index, dy, isBottom) => {
     this.setState({dragFromTop: !isBottom})
-    if (!this.state.dragging) {
-      this.setState({ dragging: true })
-    }
+    // if (!this.state.dragging) {
+    //   this.setState({ dragging: true })
+    // }
     var dragData = {...this.state.dragData}
     dragData.index = index;
     dragDistance = isBottom ? -dy : dy;
+    dragData.dragDistance = dragDistance;
     dragData.marginLeft.setValue(dragDistance / this.state.totalDragDistance * (styles.trackView.width + styles.trackView.marginRight))
     this.setState({dragData})
   }
 
-  onStopDrag = (item, index, isBottom) => {
+  onStopDrag = (item, index, isBottom, distance) => {
     if (item === null) {
       Animated.spring(this.state.dragData.marginLeft, {
         toValue: 0,
@@ -221,45 +254,66 @@ class TrackStack extends Component {
         toValue: (styles.trackView.width + styles.trackView.marginRight),
         friction: 12
       }).start(() => {
-        let tempA = isBottom ? this.state.listB : this.state.listA;
-        let tempB = isBottom ? this.state.listA : this.state.listB;
-        tempA.splice(index, 1);
-        tempB.splice(index, 0, item);
-        console.log(index, isBottom, item);
-        this.setState({
-          dragData: {
-            index: 0,
-            marginLeft: new Animated.Value(0),
-            marginAdjacent: new Animated.Value(0)
-          },
-          listA: isBottom ? tempB : tempA,
-          listB: isBottom ? tempA : tempB
-        })
-        this.state.dragData.marginAdjacent.setValue(0)
-        this.state.dragData.marginLeft.setValue(0)
-      });
-      this.setState({ dragging: false })
+        this.state.dragData.marginAdjacent.setValue(0);
+        this.state.dragData.marginLeft.setValue(0);
+        this.setState({insertTransitioning: false});
+      })
+      let tempA = isBottom ? this.state.listB : this.state.listA;
+      let tempB = isBottom ? this.state.listA : this.state.listB;
+      // let tempAOffset = isBottom ? this.state.offsets.listB : this.state.offsets.listA;
+      // let tempBOffset = isBottom ? this.state.offsets.listA : this.state.offsets.listB;
+      // let offset = index + tempAOffset - tempBOffset;
+      // console.log(index + tempAOffset - tempBOffset);
+      tempA.splice(index, 1);
+      tempB.splice(index, 0, item);
+      this.setState({
+        insertTransitioning: true,
+        listA: isBottom ? tempB : tempA,
+        listB: isBottom ? tempA : tempB,
+        dragging: false
+      })
+      // this.setState({ dragging: false })
     }
     
 
   }
 
-  handleScroll = (event) => {
-    console.log(event.nativeEvent.contentOffset.y);
+  handleScrollTop = (event, isBottom) => {
+    let distance = event.nativeEvent.contentOffset.x;
+    let offset = Math.round(distance / (styles.trackView.width + styles.trackView.marginRight));
+    if (isBottom) {
+      this.setState({
+        offsets: {
+          listA: this.state.offsets.listA,
+          listB: offset
+        }
+      });
+    } else {
+      this.setState({
+        offsets: {
+          listA: offset,
+          listB: this.state.offsets.listB
+        }
+      });
+    }
+  }
+  
+  handleScrollBottom = (event) => {
+    this.handleScrollTop(event, true);
   }
 
 
   render() {
     return (
       <View>
-        <ScrollView scrollEventThrottle={32} snapToInterval={styles.trackView.width + styles.trackView.marginRight} horizontal={true} style={styles.stack} scrollEnabled={!this.state.dragging} onScroll={this.handleScroll}>
+        <ScrollView snapToAlignment='right' snapToInterval={styles.trackView.width + styles.trackView.marginRight} horizontal={true} style={styles.stack} scrollEnabled={!this.state.dragging} onScroll={this.handleScrollTop} scrollEventThrottle={64}>
           {this.state.listA.map((item, index) => (
-            <TrackView track={item} isBottom={false} dragFrom={this.state.dragFromTop}key={item.id} i={index} dragData={this.state.dragData} onDrag={this.onDrag} onStopDrag={(item, index) => {this.onStopDrag(item, index, false)}} />
+            <TrackView transitioning={this.state.insertTransitioning} track={item} offsets={this.state.offsets} isBottom={false} dragFrom={this.state.dragFromTop} key={item.id} i={index} dragData={this.state.dragData} onDrag={this.onDrag} onStopDrag={(item, index, distance) => {this.onStopDrag(item, index, false, distance)}} />
           ))}
         </ScrollView>
-        <ScrollView scrollEventThrottle={32} snapToInterval={styles.trackView.width + styles.trackView.marginRight} horizontal={true} style={styles.stack} scrollEnabled={!this.state.dragging} onScroll={this.handleScroll}>
+        <ScrollView snapToAlignment='right' snapToInterval={styles.trackView.width + styles.trackView.marginRight} horizontal={true} style={styles.stack} scrollEnabled={!this.state.dragging} onScroll={this.handleScrollBottom} scrollEventThrottle={64}>
           {this.state.listB.map((item, index) => (
-            <TrackView track={item} isBottom={true} dragFrom={!this.state.dragFromTop} key={item.id} i={index} dragData={this.state.dragData} onDrag={this.onDrag} onStopDrag={(item, index) => {this.onStopDrag(item,index, true)}} />
+            <TrackView transitioning={this.state.insertTransitioning} track={item} offsets={this.state.offsets} isBottom={true} dragFrom={!this.state.dragFromTop} key={item.id} i={index} dragData={this.state.dragData} onDrag={this.onDrag} onStopDrag={(item, index, distance) => {this.onStopDrag(item,index, true, distance)}} />
           ))}
         </ScrollView>
       </View>
