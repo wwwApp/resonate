41d76bdf1d6c20c0e9f9da47d3815b2861dfded9
Clay Tercek <code@claytercek.com>
2019-02-20 14:39:55 -0500

    permissions and requirements setup for image picker

--- android/app/src/main/java/com/resonate/MainApplication.java
+++ android/app/src/main/java/com/resonate/MainApplication.java
@@ -3,6 +3,7 @@ package com.resonate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RNGestureHandlerPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
